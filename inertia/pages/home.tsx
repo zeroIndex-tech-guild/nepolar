ab834b57ca2e6fd5a857262c3ed43211468a7baf
haruBlank <harublank00@gmail.com>
2024-10-28 16:01:59 +0545

    feat: install and check shadcn

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,19 +1,22 @@
-import { Head } from '@inertiajs/react'
 import { useState } from 'react'
+import { Button } from '~/components/ui/button';
 
-export default function Home(props) {
+export default function Home() {
   const [count, setCount] = useState(0);
 
-  const onClick = () => {
-    setCount(prevCount => prevCount + 1);
-  }
+  const increment = () => setCount(count + 1);
 
-  console.log({ props })
+  const onClickHandler = () => {
+    increment()
+  }
 
   return (
     <>
       <h1>Hello React inertia</h1>
-      <button onClick={onClick} className='text-3xl bg-red-100'>Click me {count}</button>
+      <Button onClick={onClickHandler}>
+      Increment Count
+      </Button>
+      <span>{count}</span>
     </>
   )
 }
