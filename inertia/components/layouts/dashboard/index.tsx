4348dc42c68547dbd071f27f81d4d3f9278f08ad
haruBlank <harublank00@gmail.com>
2024-11-12 23:06:08 +0545

    feat: make dashabord layout shell

---
+++ inertia/components/layouts/dashboard/index.tsx
@@ -0,0 +1,9 @@
+export const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>hi there</h1>
+      it's layou?
+      {children}
+    </div>
+  )
+}
--- inertia/pages/challenges/index.tsx
+++ inertia/pages/challenges/index.tsx
@@ -1,4 +1,8 @@
+import { DashboardLayout } from '~/components/layouts/dashboard'
+
 export default function ChallengePage(props) {
   console.log({ props })
   return <h1>helo world</h1>
 }
+
+ChallengePage.layout = (page: React.ReactNode) => <DashboardLayout>{page}</DashboardLayout>
