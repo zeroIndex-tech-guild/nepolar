0f3f5e0b3f36141898c489dbdf2d7ab729094ba5
haruBlank <harublank00@gmail.com>
2024-11-26 23:31:27 +0545

    feat: add some style to sidebar

--- inertia/components/layouts/dashboard-sidebar/data.tsx
+++ inertia/components/layouts/dashboard-sidebar/data.tsx
@@ -1,9 +1,14 @@
-import { Swords } from 'lucide-react'
+import { Gauge, Swords } from 'lucide-react'
 
 export const dashboardSidebarItems = [
   {
     label: 'ViewPoints',
     points: [
+      {
+        title: 'Dashboard',
+        url: '/dashboard',
+        Icon: Gauge,
+      },
       {
         title: 'Challenges',
         url: '/challenges',
--- inertia/components/layouts/dashboard-sidebar/index.tsx
+++ inertia/components/layouts/dashboard-sidebar/index.tsx
@@ -9,9 +9,12 @@ import {
   SidebarMenuItem,
 } from '~/components/ui/sidebar'
 import { dashboardSidebarItems } from './data'
-import { Link } from '@inertiajs/react'
+import { Link, usePage } from '@inertiajs/react'
+import { cn } from '~/lib/utils'
 
 export const DashboardSidebar = () => {
+  const { url: pageUrl } = usePage()
+
   return (
     <Sidebar>
       <SidebarHeader>
@@ -24,16 +27,23 @@ export const DashboardSidebar = () => {
 
           return (
             <SidebarGroup key={label}>
-              <SidebarGroupLabel>{label}</SidebarGroupLabel>
+              <SidebarGroupLabel className="text-base">{label}</SidebarGroupLabel>
 
               <SidebarContent>
                 <SidebarMenu>
                   {points.map((point) => {
                     const { url, title, Icon } = point
+                    const isActive = url === pageUrl
                     return (
                       <SidebarMenuItem key={url}>
-                        <Link href={url} className="">
-                          {<Icon />}
+                        <Link
+                          href={url}
+                          className={cn(
+                            'flex items-center gap-2 text-md hover:bg-purple-800  p-2 rounded-md',
+                            isActive && 'bg-purple-800'
+                          )}
+                        >
+                          {<Icon size={16} />}
                           {title}
                         </Link>
                       </SidebarMenuItem>
