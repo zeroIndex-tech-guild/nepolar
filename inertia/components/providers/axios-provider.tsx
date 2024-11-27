6492fcea713102adca6c3a02517c0b51046422a3
haruBlank <harublank00@gmail.com>
2024-11-27 21:15:28 +0545

    comment: add comment to axios provider

--- inertia/components/providers/axios-provider.tsx
+++ inertia/components/providers/axios-provider.tsx
@@ -23,6 +23,29 @@ export const AxiosProvider = ({ children }: { children: React.ReactNode }) => {
   // Attach response interceptor
   axiosInstance.interceptors.response.use(
     (response) => {
+      /*
+       * From server we get response as
+       * {
+       *  message: string;
+       *  data: {};
+       *  error: [];
+       *  statusCode: number
+       * }
+       *
+       * with axios our response data is nested inside data
+       * so we need to do response.data to get our response
+       * i.e {message, data, error, statusCode}
+       *
+       * For some reason axios is firing twice and instead of returning
+       * response.data only (it returns this on first call)
+       * it returns response.data again which will be response.data.(our repsonse object)data
+       *
+       * hence this check
+       */
+      // first time response.data.data will be defined (it's our response.data object)
+      // after this there won't be response.data.data defined
+      // so we can just return response as it's our response object
+      // *finger cross 🤞*
       if (response && response.data.data) {
         return response.data
       }
